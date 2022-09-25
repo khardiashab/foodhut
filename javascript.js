f81d900a902bab1ff86a0721d427d3e955ff46ac
khardiashab <79443051+khardiashab@users.noreply.github.com>
2022-09-25 16:06:08 +0530

    Here I add all files

---
+++ javascript.js
@@ -0,0 +1,9 @@
+var food = document.querySelector(".food");
+var juice = document.querySelector(".juice");
+var juiceBtn = document.querySelector(".juice-btn")
+var foodBtn = document.querySelector(".food-btn")
+juiceBtn.onclick() = function(){
+    food.style.display = "none";
+    juice.style.display ="block";
+
+}
